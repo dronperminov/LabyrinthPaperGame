5361ceb265a0dfe7a4eae3121d154f4e051c13d1
Светлана Кудинова <sd_kudinova@ispras.ru>
2020-04-05 19:26:37 +0300

    Add simple ideal maze algorithm

--- generator.js
+++ generator.js
@@ -5,6 +5,7 @@ const LEFT = "left"
 const RIGHT = "right"
 const UP = "up"
 const DOWN = "down"
+const DIRECTIONS = [LEFT, RIGHT, UP, DOWN]
 
 // генератор лабиринтов
 function MazeGenerator(n, m) {
@@ -50,9 +51,82 @@ MazeGenerator.prototype.GetWallState = function(x, y, dir) {
     return this.matrix[yi][xi]
 }
 
+// получение случайной клетки лабиринта
+MazeGenerator.prototype.GetRandomCell = function() {
+    let x = Math.floor(Math.random() * this.m)
+    let y = Math.floor(Math.random() * this.n)
+
+    return {x: x, y: y}
+}
+
+// получение следующей ячейки по направлению
+MazeGenerator.prototype.GetNextCell = function(cell, direction) {
+    let x = cell.x + this.dirsX[direction]
+    let y = cell.y + this.dirsY[direction]
+
+    return {x: x, y: y}
+}
+
+// получение возможных направлений
+MazeGenerator.prototype.GetPosibleDirections = function(cell) {
+    let directions = []
+
+    if (cell.x > 0)
+        directions.push(LEFT)
+
+    if (cell.x < this.m - 1)
+        directions.push(RIGHT)
+
+    if (cell.y > 0)
+        directions.push(UP)
+
+    if (cell.y < this.n - 1)
+        directions.push(DOWN)
+
+    return directions
+}
+
+// получение случайного направления
+MazeGenerator.prototype.GetRandomDirection = function(directions = DIRECTIONS) {
+    return directions[Math.floor(Math.random() * directions.length)]
+}
+
+// проверка на посещение всех вершин
+MazeGenerator.prototype.IsAllVisited = function(visited) {
+    for (let i = 0; i < this.n; i++)
+        for (let j = 0; j < this.m; j++)
+            if (!visited[i][j])
+                return false
+
+    return true
+}
+
 // генерация лабиринта
 MazeGenerator.prototype.Generate = function(type) {
+    let visited = []
 
+    for (let i = 0; i < this.n; i++) {
+        visited[i] = []
+
+        for (let j = 0; j < this.m; j++)
+            visited[i][j] = false
+    }
+
+    let cell = this.GetRandomCell()
+    visited[cell.y][cell.x] = true
+
+    while (!this.IsAllVisited(visited)) {
+        let directions = this.GetPosibleDirections(cell)
+        let direction = this.GetRandomDirection(directions)
+        let newCell = this.GetNextCell(cell, direction)
+
+        if (!visited[newCell.y][newCell.x]) {
+            visited[newCell.y][newCell.x] = true
+            this.SetWallState(cell.x, cell.y, direction, EMPTY_CELL)
+        }
+
+        cell = newCell
+    }
 }
 
 // добавление стен в лабиринт
